15e03bbb0651efe4eeaeed306748cae4539ebe2e
홍수진 <shineetm0718@naver.com>
2023-07-13 22:20:38 +0900

    깃 lfs 설치 후 대용량 파일 커밋
    Former-commit-id: 641cda46f90f26a8b67ae4bc3d6d9abca258e762 [formerly 16ae37df60229b42888dae55740520e3ab0158f6]
    Former-commit-id: 25499ee05c001e3cb918ecdecd6ee3634fed7c94

--- Assets/@Scripts/Managers/Contents/ObjectManager.cs
+++ Assets/@Scripts/Managers/Contents/ObjectManager.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectManager : MonoBehaviour
+public class ObjectManager
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/@Scripts/Scenes/GameScene.cs
+++ Assets/@Scripts/Scenes/GameScene.cs
@@ -13,6 +13,11 @@ public class GameScene : MonoBehaviour
     
     void Start()
     {
+        // 비동기 방식
+        // 일처리를 맡겨놓고 다른 부분 진행
+        // 끝난 후 Callback 함수 실행
+        // "야 무슨 일 있으면 연락 줘!"
+        // 코드 효율성 Up, 어려워짐
         Managers.Resource.LoadAllAsync<GameObject>("Prefabs", (key, count, totalCount) =>
         {
             Debug.Log($"{key} {count}/{totalCount}");
@@ -28,11 +33,17 @@ public class GameScene : MonoBehaviour
 
     void StartLoaded()
     {
-        GameObject go = new GameObject() { name = "@Monsters" };
+        var player = Managers.Resource.Instantiate("Slime_01.prefab");
+        player.AddComponent<PlayerController>();
 
-        //_slime.AddComponent<PlayerController>();
+        var snake = Managers.Resource.Instantiate("Snake_01.prefab");
+        var goblin = Managers.Resource.Instantiate("Goblin_01.prefab");
+        var joystick = Managers.Resource.Instantiate("UI_Joystick.prefab");
+        joystick.name = "@UI_Joystick";
 
-        Camera.main.GetComponent<CameraController>().Target = _slime;
+        var map = Managers.Resource.Instantiate("Map.prefab");
+        map.name = "@Map";
+        Camera.main.GetComponent<CameraController>().Target = player;
     }
 
     void Update()
